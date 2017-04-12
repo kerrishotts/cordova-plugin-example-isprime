c7fd23279a23da962274b07ee8d5490020bcd913
Kerri Shotts <kerrishotts@gmail.com>
2017-04-12 16:25:13 -0500

    try this

--- tests/travis.sh
+++ tests/travis.sh
@@ -18,9 +18,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
     emulator -avd test -no-audio -no-window &
     android-wait-for-emulator
-    adb shell input keyevent 82 &
+    #adb shell input keyevent 82 &
     npm run test:android
     ps -eaf | grep emulator | grep -v grep | cut -d " " -f 4 | xargs kill -9
     ps -eaf | grep qemu | grep -v grep | cut -d " " -f 4 | xargs kill -9
-    ps -eaf | grep adb | grep -v grep | cut -d " " -f 4 | xargs kill -9
+    #ps -eaf | grep adb | grep -v grep | cut -d " " -f 4 | xargs kill -9
 fi
