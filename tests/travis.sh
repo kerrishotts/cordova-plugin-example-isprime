6a3bc716bfd4047005f33a0092f20d889b5d910a
Kerri Shotts <kerrishotts@gmail.com>
2017-05-05 13:38:57 -0500

    try sudo

--- tests/travis.sh
+++ tests/travis.sh
@@ -10,7 +10,7 @@ npm run lint
 
 # run tests appropriate for platform
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    gem install cocoapods
+    sudo gem install cocoapods
     npm install -g ios-sim ios-deploy
     npm run test:ios
 fi
@@ -19,9 +19,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
     emulator -avd test -no-audio -no-window &
     android-wait-for-emulator
-    #adb shell input keyevent 82 &
     npm run test:android
-    # ps -eaf -ww
+    # Apparently no longer need to kill things in latest Travis
     # killall -9 -q emulator64-arm
     # killall -9 -q emulator64-crash-service
     # killall -9 -q adb
