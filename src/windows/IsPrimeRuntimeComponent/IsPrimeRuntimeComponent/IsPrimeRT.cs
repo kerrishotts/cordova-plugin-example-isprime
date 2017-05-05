3189e3cc59f18fbe4a1e8930235d9ea89710429e
Kerri Shotts <kerrishotts@gmail.com>
2017-05-04 21:32:13 -0500

    Adding C# for Windows

---
+++ src/windows/IsPrimeRuntimeComponent/IsPrimeRuntimeComponent/IsPrimeRT.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsPrimeRuntimeComponent
+{
+    public sealed class IsPrimeRT
+    {
+        static public long[] Batch(long candidate, long startAt, long batchSize, long endAt)
+        {
+            long stopAt = Math.Min(startAt + batchSize - 1, endAt);
+            List<long> results = new List<long>();
+            if (candidate == 2)
+            {
+                return results.ToArray<long>();
+            }
+            for (long i = startAt; i <= stopAt; i++)
+            {
+                if ((candidate % i) == 0)
+                {
+                    results.Add(i);
+                }
+            }
+            return results.ToArray<long>();
+        }
+    }
+}
