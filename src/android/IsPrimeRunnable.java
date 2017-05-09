32072e032b09872c74ea29611c1870baa300ee23
Kerri Shotts <kerrishotts@gmail.com>
2017-05-08 21:32:31 -0500

    fix copy and paste!

--- src/android/IsPrimeRunnable.java
+++ src/android/IsPrimeRunnable.java
@@ -1,112 +1,94 @@
 package com.kerrishotts.example.isprime;
 
-import java.util.ArrayList;
-import java.util.concurrent.BlockingQueue;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.GregorianCalendar;
 
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
+public class IsPrimeRunnable implements Runnable {
 
+    private final JSONObject _result;
+    private final CallbackContext _context;
+    private volatile long _lastCandidate = 0;
 
-/**
-* This class exposes methods in Cordova that can be called from JavaScript.
-*/
-public class IsPrime extends CordovaPlugin {
-
-    public static final int START_THREADS = 4;
-    public static final int MAX_THREADS = 8;
-    public static final int KEEP_ALIVE = 1000;
-
-
-    private InspectableThreadPoolExecutor _pool;
-    private BlockingQueue<Runnable> _q;
-    private ArrayList<Runnable> _pausedTasks;
-
-    private void _initPool() {
-
-        _q = new ArrayBlockingQueue<Runnable>(MAX_THREADS);
-        _pool = new InspectableThreadPoolExecutor(START_THREADS, MAX_THREADS, KEEP_ALIVE, TimeUnit.MILLISECONDS, _q);
+    public IsPrimeRunnable(final JSONObject result, final CallbackContext callbackContext) {
+        _result = result;
+        _context = callbackContext;
+        _lastCandidate = 2; // this is where we start checking -- not one or zero.
+    }
+    private IsPrimeRunnable(final JSONObject result, final CallbackContext callbackContext, long lastCandidate) {
+        _result = result;
+        _context = callbackContext;
+        _lastCandidate = lastCandidate; // start from last checked candidate
     }
 
-    private void _destroyPool() {
-        if (_pool != null) {
-            _pool.shutdownNow();
+    public IsPrimeRunnable copy() {
+        return new IsPrimeRunnable(_result, _context, _lastCandidate);
+    }
+
+    public void run() {
+        try {
+            JSONArray factors = _result.getJSONArray("factors");
+            long candidate = _result.getLong("candidate");
+            long half = candidate / 2;
+            long now = (new GregorianCalendar()).getTimeInMillis();
+            long cur = now;
+            long start = _lastCandidate;
+
+            if (candidate == 2) {
+                _result.put("progress", 100);
+                _result.put("complete", true);
+                _result.put("isPrime", true);
+            } else {
+                // we are divisible by ourselves
+                if (start <= 2) {
+                    factors.put(1);
+                }
+                // if we were only interested in testing primality, we could get by
+                // with sqrt(candidate), but since we want the factors, we have to
+                // go up to candidate/2.
+                for (long i = start; i<=half; i++) {
+                    if (i % 1000 == 0) {
+                        _result.put("progress", ((double)i / (double)half) * 100);
+                        cur = (new GregorianCalendar()).getTimeInMillis();
+                        if (cur - now > 1000) {
+                            now = cur;
+                            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, _result);
+                            pluginResult.setKeepCallback(true);
+                            _context.sendPluginResult(pluginResult);
+                        }
+
+                        // check if we've been interrupted
+                        if (Thread.currentThread().isInterrupted()) {
+                            throw new InterruptedException();
+                        }
+                    }
+                    if ((candidate % i) == 0) {
+                        factors.put(i);
+                    }
+                    _lastCandidate = i;
+                }
+                if (factors.length() == 1) {
+                    // no factors, so we're prime
+                    _result.put("isPrime", true);
+                    // since we're prime, clear out factors
+                    factors.remove(0);
+                } else {
+                    // we are divisible by ourselves as well
+                    factors.put(candidate);
+                }
+            }
+            _result.put("progress", 100);
+            _result.put("complete", true);
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, _result);
+            _context.sendPluginResult(pluginResult);
+        } catch (JSONException e) {
+            _context.error("JSON Exception; check that the JS API is passing the right result object");
+        } catch (InterruptedException e) {
+            // do nothing; we'll get taken care of later
         }
-    }
-
-    private void _pausePool() {
-        _pausedTasks = new ArrayList<Runnable>(MAX_THREADS);
-        _pausedTasks.addAll(_pool.getAllTasks());
-        _pool.shutdownNow();
-    }
-
-    private void _restartPool() {
-        _initPool();
-        Runnable[] arr = _pausedTasks.toArray(new Runnable[0]);
-        for (int i = 0; i < arr.length; i++) {
-            _pool.execute(((IsPrimeRunnable)arr[i]).copy());
-        }
-
-        _pausedTasks = null;
-    }
-
-    @Override
-    public void initialize(org.apache.cordova.CordovaInterface cordova, org.apache.cordova.CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        _initPool();
-    }
-
-    @Override
-    public void onDestroy() {
-        _destroyPool();
-    }
-
-    public void onReset() {
-        _destroyPool();
-        _initPool();
-        if (_q != null) {
-            _q = null;
-        }
-    }
-
-    @Override
-    public void onPause(boolean multitasking) {
-        _pausePool();
-    }
-
-    @Override
-    public void onResume(boolean multitasking) {
-        _restartPool();
-    }
-
-     /**
-     * Executes the request and returns PluginResult.
-     *
-     * @param action            The action to execute.
-     * @param args              JSONArry of arguments for the plugin.
-     * @param callbackContext   The callback context from which we were invoked.
-     */
-    @SuppressLint("NewApi")
-    @Override
-    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        if ("isPrime".equals(action)) {
-            this.isPrime(args.getJSONObject(0), callbackContext);
-        } else {
-            return false;
-        }
-        return true;
-    }
-
-    private void isPrime(final JSONObject result, final CallbackContext callbackContext) throws JSONException {
-        _pool.execute(new IsPrimeRunnable(result, callbackContext));
     }
 }
