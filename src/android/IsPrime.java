8891a02750ccc87d7161b66d7a92369b32c5baa7
Kerri Shotts <kerrishotts@gmail.com>
2017-05-17 15:57:02 +0200

    add background mode

--- src/android/IsPrime.java
+++ src/android/IsPrime.java
@@ -1,96 +1,20 @@
 package com.kerrishotts.example.isprime;
 
-import java.util.ArrayList;
-import java.util.concurrent.BlockingQueue;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
+import android.util.Base64;
 
-
-/**
-* This class exposes methods in Cordova that can be called from JavaScript.
-*/
 public class IsPrime extends CordovaPlugin {
 
-    public static final int START_THREADS = 0;
-    public static final int MAX_THREADS = 8;
-    public static final int KEEP_ALIVE = 1000;
-
-
-    private InspectableThreadPoolExecutor _pool;
-    private BlockingQueue<Runnable> _q;
-    private ArrayList<Runnable> _pausedTasks;
-
-    private void _initPool() {
-
-        _q = new ArrayBlockingQueue<Runnable>(MAX_THREADS);
-        _pool = new InspectableThreadPoolExecutor(START_THREADS, MAX_THREADS, KEEP_ALIVE, TimeUnit.MILLISECONDS, _q);
-    }
-
-    private void _destroyPool() {
-        if (_pool != null) {
-            _pool.shutdownNow();
-        }
-    }
-
-    private void _pausePool() {
-        _pausedTasks = new ArrayList<Runnable>(MAX_THREADS);
-        _pausedTasks.addAll(_pool.getAllTasks());
-        _pool.shutdownNow();
-    }
-
-    private void _restartPool() {
-        _initPool();
-        Runnable[] arr = _pausedTasks.toArray(new Runnable[0]);
-        for (int i = 0; i < arr.length; i++) {
-            _pool.execute(((IsPrimeRunnable)arr[i]).copy());
-        }
-
-        _pausedTasks = null;
-    }
-
-    @Override
-    protected void pluginInitialize() {
-        _initPool();
-    }
-
-    @Override
-    public void onDestroy() {
-        _destroyPool();
-    }
-
-    public void onReset() {
-        _destroyPool();
-        _initPool();
-    }
-
-    @Override
-    public void onPause(boolean multitasking) {
-        _pausePool();
-    }
-
-    @Override
-    public void onResume(boolean multitasking) {
-        _restartPool();
-    }
-
-     /**
-     * Executes the request and returns PluginResult.
-     *
-     * @param action            The action to execute.
-     * @param args              JSONArry of arguments for the plugin.
-     * @param callbackContext   The callback context from which we were invoked.
-     */
     @SuppressLint("NewApi")
     @Override
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -103,6 +27,58 @@ public class IsPrime extends CordovaPlugin {
     }
 
     private void isPrime(final JSONObject result, final CallbackContext callbackContext) throws JSONException {
-        _pool.execute(new IsPrimeRunnable(result, callbackContext));
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+                    JSONArray factors = result.getJSONArray("factors");
+                    long candidate = result.getLong("candidate");
+                    long half = candidate / 2;
+                    long now = (new GregorianCalendar()).getTimeInMillis();
+                    long cur = now;
+
+                    if (candidate == 2) { // [1]
+                        result.put("progress", 100);
+                        result.put("complete", true);
+                        result.put("isPrime", true);
+                    } else {
+                        factors.put(1); // [2a]
+                        for (long i = 2; i<=half; i++) {
+                            if (i % 1000 == 0) {
+                                result.put("progress", ((double)i / (double)half) * 100);
+                                cur = (new GregorianCalendar()).getTimeInMillis();
+                                if (cur - now > 1000) { // [3]
+                                    now = cur;
+                                    // [4]
+                                    PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+                                    // [5]
+                                    pluginResult.setKeepCallback(true);
+                                    // [6]
+                                    callbackContext.sendPluginResult(pluginResult);
+                                }
+                            }
+                            if ((candidate % i) == 0) {
+                                factors.put(i);
+                            }
+                        }
+                        if (factors.length() == 1) {
+                            result.put("isPrime", true);
+                            factors.remove(0); // [2b]
+                        } else {
+                            factors.put(candidate);
+                        }
+                    }
+                    result.put("progress", 100);
+                    result.put("complete", true);
+                    // [4]
+                    PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+                    // [6]
+                    callbackContext.sendPluginResult(pluginResult);
+
+                } catch (JSONException e) {
+                    callbackContext.error("JSON Exception; check that the JS API is passing the right result object");
+                }
+            }
+        });
     }
+
 }
